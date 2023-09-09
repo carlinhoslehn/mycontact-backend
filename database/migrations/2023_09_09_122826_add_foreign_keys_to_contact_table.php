aa59eb11be5e5e1321fec566abbcea3ac37ec237
Carlos Eduardo <dudunh@gmail.com>
2023-09-09 11:08:17 -0300

    Iniciando o backend com laravel 10

---
+++ database/migrations/2023_09_09_122826_add_foreign_keys_to_contact_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('contacts', function (Blueprint $table) {
+
+            $table->unsignedBigInteger('category_id')
+            ->after('id')
+            ->nullable(false);
+
+            $table->foreign('category_id')
+            ->references('id')
+            ->on('categories')
+            ->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('contacts', function (Blueprint $table) {
+            $table->dropForeign(['category_id']);
+        });
+    }
+};
